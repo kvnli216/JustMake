0725e70b034ff28fe842e34def3a132fd6a29dab
kvnli216 <li.ctrl.z@gmail.com>
2019-04-22 19:36:02 -0400

    Implement mock project data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,29 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      client: io('http://localhost:1337'),
+
+    };
+
+    this.gameState = {
+      players: [
+        {
+          hand: [
+            { value: 'A', suit: 'S' },
+            { value: 'J', suit: 'S' },
+            { value: '3', suit: 'H' },
+            { value: 'K', suit: 'C' },
+            { value: '2', suit: 'D' },
+          ],
+          bid: 2,
+          username: 'player',
+          avatar: 'image',
+          scores: [1, 1, 3],
+          playedCard: { value: 'A', suit: 'S' }, // or null
+        },
+      ],
+      trump: 'S', // or null
+      currentPlayerTurn: 0, // reference to player index
+      gameState: 'game setup/middle/end of game',
     };
   }
 
